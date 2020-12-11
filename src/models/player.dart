9dd42fe0a8603d055d898cb8bfa938cd330f5c86
gianfcm <you@example.com>
2020-12-11 17:06:28 -0300

    remove

--- src/models/player.dart
+++
@@ -1,6 +0,0 @@
-import '../shared/types/piece_color.dart';
-
-class Player {
-  String username;
-  PIECE_COLOR piece_color;
-}
--- src/models/turn.dart
+++
@@ -1,9 +0,0 @@
-class Turn {
-  String board_id;
-  String turn_token;
-  String username;
-  String actual_turn;
-  String board;
-  int move_left;
-  String opponent_username;
-}
