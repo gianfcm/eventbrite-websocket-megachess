8d1ecae6181f80b174240f8de6a191780b9becaf
gianfcm <you@example.com>
2020-12-14 18:16:46 -0300

    auth

--- bin/eventbrite-websocket-megachess.dart
+++ bin/eventbrite-websocket-megachess.dart
@@ -1,11 +1,12 @@
+import 'dart:io';
+
 import '../src/classes/websocket/chess_socket.dart';
 
 void main(List<String> arguments) {
   if (arguments.isEmpty) {
     print('please provide your auth_token');
-    //exit(0); poner esto despues
+    exit(0); 
   }
-  //final authToken = arguments[0];
-  //print('authToken: $authToken');
-  ChessSocket(authToken: 'f98e6c31-012d-4a77-9436-719601cd8f1f');
+  final authToken = arguments[0];
+  ChessSocket(authToken: authToken);
 }
